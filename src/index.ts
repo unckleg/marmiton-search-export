271cfc22d04869e3a0a3adad11e651759d3446a1
Djordje Stojiljkovic <djordjestojilljkovic@gmail.com>
2022-12-04 10:16:19 +0100

    Fixed linting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-"phantombuster command: nodejs"
-"phantombuster package: 5"
+'phantombuster command: nodejs';
+'phantombuster package: 5';
 
-;(async () => {
-  console.log("Hello from the ether side!")
-  process.exit()
-})()
+(async () => {
+  console.log('Hello from the ether side!');
+  process.exit();
+})();
